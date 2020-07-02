172fc43eb56a3e115f8fd403319df30a713825de
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-07-02 18:45:19 +0200

    refactor: move swagger to openapi

---
+++ defectdojo_openapi/api/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from defectdojo_openapi.api.api_token_auth_api import ApiTokenAuthApi
+from defectdojo_openapi.api.development_environments_api import DevelopmentEnvironmentsApi
+from defectdojo_openapi.api.endpoints_api import EndpointsApi
+from defectdojo_openapi.api.engagements_api import EngagementsApi
+from defectdojo_openapi.api.finding_templates_api import FindingTemplatesApi
+from defectdojo_openapi.api.findings_api import FindingsApi
+from defectdojo_openapi.api.import_scan_api import ImportScanApi
+from defectdojo_openapi.api.jira_configurations_api import JiraConfigurationsApi
+from defectdojo_openapi.api.jira_finding_mappings_api import JiraFindingMappingsApi
+from defectdojo_openapi.api.jira_product_configurations_api import JiraProductConfigurationsApi
+from defectdojo_openapi.api.metadata_api import MetadataApi
+from defectdojo_openapi.api.note_type_api import NoteTypeApi
+from defectdojo_openapi.api.notes_api import NotesApi
+from defectdojo_openapi.api.product_types_api import ProductTypesApi
+from defectdojo_openapi.api.products_api import ProductsApi
+from defectdojo_openapi.api.reimport_scan_api import ReimportScanApi
+from defectdojo_openapi.api.scan_settings_api import ScanSettingsApi
+from defectdojo_openapi.api.scans_api import ScansApi
+from defectdojo_openapi.api.stub_findings_api import StubFindingsApi
+from defectdojo_openapi.api.system_settings_api import SystemSettingsApi
+from defectdojo_openapi.api.test_types_api import TestTypesApi
+from defectdojo_openapi.api.tests_api import TestsApi
+from defectdojo_openapi.api.tool_configurations_api import ToolConfigurationsApi
+from defectdojo_openapi.api.tool_product_settings_api import ToolProductSettingsApi
+from defectdojo_openapi.api.tool_types_api import ToolTypesApi
+from defectdojo_openapi.api.users_api import UsersApi
